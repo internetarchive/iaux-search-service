c616edd8df11890225387939cdc7b08778378449
Jason Buckner <jason@jasonbuckner.com>
2021-01-12 08:53:26 -0800

    Use shared parser for boolean

--- src/models/metadata-fields/field-types/boolean.ts
+++ src/models/metadata-fields/field-types/boolean.ts
@@ -2,6 +2,10 @@ import { FieldParserInterface } from '../field-parser-interface';
 import { MetadataField } from '../metadata-field';
 
 export class BooleanParser implements FieldParserInterface<boolean> {
+  // use a shared static instance for performance instead of
+  // instantiating a new instance for every use
+  static shared = new BooleanParser();
+
   parseValue(rawValue: string): boolean {
     if (rawValue === 'false' || rawValue === '0') {
       return false;
@@ -13,7 +17,6 @@ export class BooleanParser implements FieldParserInterface<boolean> {
 export class BooleanField extends MetadataField<boolean, BooleanParser> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   constructor(rawValue: any) {
-    const parser = new BooleanParser();
-    super(parser, rawValue);
+    super(BooleanParser.shared, rawValue);
   }
 }
