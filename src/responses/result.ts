c544551ec110ce85d20da8e7792be0aafc1a2329
Jason Buckner <jason@jasonbuckner.com>
2021-01-12 09:05:59 -0800

    Update comment on Result object

--- src/responses/result.ts
+++ src/responses/result.ts
@@ -1,8 +1,12 @@
 /**
  * The Result is a container for a response.
  *
- * It contains an optional success result which is generic and can be anything depending on
- * the context, or an Error or subclass of an error.
+ * It contains an optional success result which is generic
+ * and can be anything depending on the context,
+ * or an Error or subclass of an error.
+ *
+ * This allows us to return rich, typed errors instead of
+ * an untyped Promise rejection.
  *
  * This is modeled after Swift's Result type:
  * https://developer.apple.com/documentation/swift/result
