04f6cc7b4f5f3266a2a3e5f01f2a8f0bf4272fbc
Laton Vermette <1619661+latonv@users.noreply.github.com>
2022-08-30 15:56:09 -0700

    Add hit factory and merge type for hits

---
+++ src/models/hit-types/hit.ts
@@ -0,0 +1,42 @@
+import { ItemHit } from "./item-hit";
+import { TextHit } from "./text-hit";
+
+/**
+ * Union of the different hit_type values returned by the PPS.
+ * There will probably be more of these.
+ */
+export type HitType = 'item' | 'text';
+
+/**
+ * A type having all properties that exist on either T or U.
+ */
+type Merge<T, U> = {
+  [K in (keyof T | keyof U)]: (
+    K extends keyof T ? T[K] :
+    K extends keyof U ? U[K] :
+    never
+  )
+};
+
+/**
+ * Hit is an expansive type definition encompassing all the optional 
+ * and required properties that may occur on any type of hit returned 
+ * by the various search backends. (Most metadata properties are
+ * optional anyway).
+ */
+export type Hit = Partial<Merge<ItemHit, TextHit>>;
+
+export class HitFactory {
+  private constructor() {}
+
+  static createFromType(hit: Hit, type: HitType): Hit {
+    switch (type) {
+      case 'item':
+        return new ItemHit(hit);
+      case 'text':
+        return new TextHit(hit);
+      default:
+        throw new Error(`Unrecognized hit type: ${type}`);
+    }
+  }
+}
