d0f480a03143d231fb1a0da5c5a9cb56f95ca283
Laton Vermette <1619661+latonv@users.noreply.github.com>
2022-09-02 14:04:07 -0700

    Memoize search backends to avoid redundancy

--- src/search-backend/search-backend-factory.ts
+++ src/search-backend/search-backend-factory.ts
@@ -3,6 +3,7 @@ import { MetadataSearchBackend } from './metadata-search-backend';
 import { SearchType } from '../search-type';
 import type { SearchBackendInterface } from './search-backend-interface';
 import type { SearchBackendOptions } from './search-backend-options';
+import { Memoize } from 'typescript-memoize';
 
 /**
  * A factory to obtain the correct search backend for a given search type.
@@ -17,6 +18,10 @@ export class SearchBackendFactory {
    * @param type The type of search that the backend needs to handle.
    * @param options Options to pass to the search backend.
    */
+  @Memoize((type: SearchType, options: SearchBackendOptions = {}) => {
+    // We can memoize backends based on their params, to avoid constructing redundant backends
+    return `${type};${options.includeCredentials ?? ''};${options.scope ?? ''};${options.baseUrl ?? ''}`;
+  })
   static getBackendForSearchType(
     type: SearchType,
     options: SearchBackendOptions = {}
