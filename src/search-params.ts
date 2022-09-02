6d255bb9b01e234c2a6da3be55d5027afcf1d60c
Laton Vermette <1619661+latonv@users.noreply.github.com>
2022-09-02 14:17:06 -0700

    Better document search params

--- src/search-params.ts
+++ src/search-params.ts
@@ -22,9 +22,10 @@ export interface SortParam {
  * SearchParams provides an encapsulation to all of the search parameters
  * available for searching.
  *
- * It also provides an `asUrlSearchParams` method for converting the
- * parameters to an IA-style query string. ie. it converts the `fields` array
- * to `fl=identifier,collection` and `sort` to `sort=date+desc,downloads+asc`
+ * The `SearchParamURLGenerator.generateUrlSearchParams` method can be used
+ * for converting the parameters to an IA-style query string -- i.e., it 
+ * converts the `fields` array to `fields=identifier,collection` and `sort` to 
+ * `sort=date:desc,downloads:asc`
  */
 export interface SearchParams {
   query: string;
