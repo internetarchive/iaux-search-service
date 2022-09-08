8b5421ff36976f4098a19a6b8305f4c3fa38b750
Laton Vermette <1619661+latonv@users.noreply.github.com>
2022-09-08 14:38:12 -0700

    Move backend factory method onto SearchService

--- src/search-service.ts
+++ src/search-service.ts
@@ -4,7 +4,11 @@ import { SearchServiceError } from './search-service-error';
 import type { SearchServiceInterface } from './search-service-interface';
 import type { Result } from '@internetarchive/result-type';
 import { SearchType } from './search-type';
-import { SearchBackendFactory } from './search-backend/search-backend-factory';
+import { SearchBackendOptions } from './search-backend/search-backend-options';
+import { SearchBackendInterface } from './search-backend/search-backend-interface';
+import { FulltextSearchBackend } from './search-backend/fulltext-search-backend';
+import { MetadataSearchBackend } from './search-backend/metadata-search-backend';
+import { Memoize } from 'typescript-memoize';
 
 /**
  * The Search Service is responsible for taking the raw response provided by
@@ -22,7 +26,7 @@ export class SearchService implements SearchServiceInterface {
     params: SearchParams,
     searchType: SearchType = SearchType.METADATA
   ): Promise<Result<SearchResponse, SearchServiceError>> {
-    const searchBackend = SearchBackendFactory.getBackendForSearchType(
+    const searchBackend = SearchService.getBackendForSearchType(
       searchType
     );
 
@@ -33,5 +37,30 @@ export class SearchService implements SearchServiceInterface {
 
     const modeledResponse = new SearchResponse(rawResponse.success);
     return { success: modeledResponse };
+  }
+
+  /**
+   * Retrieve a search backend that can handle the given type of search.
+   * @param type The type of search that the backend needs to handle.
+   * @param options Options to pass to the search backend.
+   */
+  @Memoize((type: SearchType, options: SearchBackendOptions = {}) => {
+    // We can memoize backends based on their params, to avoid constructing redundant backends
+    const { includeCredentials = '', scope = '', baseUrl = '' } = options;
+    return `${type};${includeCredentials};${scope};${baseUrl}`;
+  })
+  private static getBackendForSearchType(
+    type: SearchType,
+    options: SearchBackendOptions = {}
+  ): SearchBackendInterface {
+    switch (type) {
+      case SearchType.TV: // Will eventually have its own service backend
+      case SearchType.RADIO: // Will eventually have its own service backend
+      case SearchType.FULLTEXT:
+        return new FulltextSearchBackend(options);
+      case SearchType.METADATA:
+      default:
+        return new MetadataSearchBackend(options);
+    }
   }
 }
