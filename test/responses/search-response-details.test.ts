76629ba8140e89ab4281604de57db1acaef300dd
Laton Vermette <1619661+latonv@users.noreply.github.com>
2023-07-10 20:41:57 -0700

    Test fixes

--- test/responses/search-response-details.test.ts
+++ test/responses/search-response-details.test.ts
@@ -67,7 +67,7 @@ describe('SearchResponseDetails', () => {
   });
 
   it('prefers hit-type specified on hit itself over schema hit-type', () => {
-    const responseBodyWithTextHit = {...responseBody} as SearchResponseBody;
+    const responseBodyWithTextHit = { ...responseBody } as SearchResponseBody;
     responseBodyWithTextHit.hits = {
       total: 2,
       returned: 2,
@@ -156,7 +156,7 @@ describe('SearchResponseDetails', () => {
   });
 
   it('collection titles map is optional', () => {
-    const responseBodyWithoutTitles = {...responseBody} as SearchResponseBody;
+    const responseBodyWithoutTitles = { ...responseBody } as SearchResponseBody;
     delete responseBodyWithoutTitles.collection_titles;
 
     const responseSchema = {
@@ -184,7 +184,9 @@ describe('SearchResponseDetails', () => {
   });
 
   it('collection extra info is optional', () => {
-    const responseBodyWithoutExtraInfo = {...responseBody} as SearchResponseBody;
+    const responseBodyWithoutExtraInfo = {
+      ...responseBody,
+    } as SearchResponseBody;
     delete responseBodyWithoutExtraInfo.collection_extra_info;
 
     const responseSchema = {
