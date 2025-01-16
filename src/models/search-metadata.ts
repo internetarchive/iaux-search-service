3c308b68bec6b93b2e334a54578490249476fefa
Jason Buckner <jason@jasonbuckner.com>
2025-01-16 12:08:35 -0800

    Add null checks

--- src/models/search-metadata.ts
+++ src/models/search-metadata.ts
@@ -9,7 +9,7 @@ import { Memoize } from 'typescript-memoize';
 
 export class SearchMetadata extends Metadata {
   @Memoize() get capture_dates(): DateField | undefined {
-    return this.rawMetadata.capture_dates
+    return this.rawMetadata.capture_dates != null
       ? new DateField(this.rawMetadata.capture_dates)
       : undefined;
   }
@@ -25,7 +25,7 @@ export class SearchMetadata extends Metadata {
    */
   /** Optional. */
   @Memoize() get created_on(): DateField | undefined {
-    return this.rawMetadata.created_on
+    return this.rawMetadata.created_on != null
       ? new DateField(this.rawMetadata.created_on)
       : undefined;
   }
@@ -37,25 +37,25 @@ export class SearchMetadata extends Metadata {
   }
 
   @Memoize() get filename(): StringField | undefined {
-    return this.rawMetadata.filename
+    return this.rawMetadata.filename != null
       ? new StringField(this.rawMetadata.filename)
       : undefined;
   }
 
   @Memoize() get file_basename(): StringField | undefined {
-    return this.rawMetadata.file_basename
+    return this.rawMetadata.file_basename != null
       ? new StringField(this.rawMetadata.file_basename)
       : undefined;
   }
 
   @Memoize() get genre(): StringField | undefined {
-    return this.rawMetadata.genre
+    return this.rawMetadata.genre != null
       ? new StringField(this.rawMetadata.genre)
       : undefined;
   }
 
   @Memoize() get indexflag(): StringField | undefined {
-    return this.rawMetadata.indexflag
+    return this.rawMetadata.indexflag != null
       ? new StringField(this.rawMetadata.indexflag)
       : undefined;
   }
@@ -79,13 +79,13 @@ export class SearchMetadata extends Metadata {
   }
 
   @Memoize() get lending___status(): StringField | undefined {
-    return this.rawMetadata.lending___status
+    return this.rawMetadata.lending___status != null
       ? new StringField(this.rawMetadata.lending___status)
       : undefined;
   }
 
   @Memoize() get licenseurl(): StringField | undefined {
-    return this.rawMetadata.licenseurl
+    return this.rawMetadata.licenseurl != null
       ? new StringField(this.rawMetadata.licenseurl)
       : undefined;
   }
@@ -114,7 +114,7 @@ export class SearchMetadata extends Metadata {
    */
   /** Optional. */
   @Memoize() get query(): StringField | undefined {
-    return this.rawMetadata.query
+    return this.rawMetadata.query != null
       ? new StringField(this.rawMetadata.query)
       : undefined;
   }
@@ -123,7 +123,7 @@ export class SearchMetadata extends Metadata {
    * Optional.
    */
   @Memoize() get date_favorited(): DateField | undefined {
-    return this.rawMetadata.date_favorited
+    return this.rawMetadata.date_favorited != null
       ? new DateField(this.rawMetadata.date_favorited)
       : undefined;
   }
@@ -133,7 +133,7 @@ export class SearchMetadata extends Metadata {
    * Start time for TV hit.
    */
   @Memoize() get start(): StringField | undefined {
-    return this.rawMetadata.start
+    return this.rawMetadata.start != null
       ? new StringField(this.rawMetadata.start)
       : undefined;
   }
@@ -142,13 +142,13 @@ export class SearchMetadata extends Metadata {
    * Optional.
    */
   @Memoize() get updated_on(): DateField | undefined {
-    return this.rawMetadata.updated_on
+    return this.rawMetadata.updated_on != null
       ? new DateField(this.rawMetadata.updated_on)
       : undefined;
   }
 
   @Memoize() get url(): StringField | undefined {
-    return this.rawMetadata.url
+    return this.rawMetadata.url != null
       ? new StringField(this.rawMetadata.url)
       : undefined;
   }
@@ -157,7 +157,7 @@ export class SearchMetadata extends Metadata {
    * Optional.
    */
   @Memoize() get __href__(): StringField | undefined {
-    return this.rawMetadata.__href__
+    return this.rawMetadata.__href__ != null
       ? new StringField(this.rawMetadata.__href__)
       : undefined;
   }
